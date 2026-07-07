d82e8efd07d03d06503c6250d7435035e089de8d
samuel wingester <sam@tenet.sator>
2026-07-07 17:42:12 -0300

    Adição de acesors, da model Users, para as models: Student e Teacher

--- backend/app/Models/Teacher.php
+++ backend/app/Models/Teacher.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 use App\Models\User;
 use App\Models\Classroom;
@@ -18,6 +19,9 @@ class Teacher extends Model
         'user_id'
     ];
 
+    //--------------------------------------------------------
+    // Relacionamentos
+    //--------------------------------------------------------
     public function user()
     {
         return $this->belongsTo( User::class, 'user_id' );
@@ -27,4 +31,44 @@ class Teacher extends Model
     {
         return $this->hasMany( Classroom::class );
     }
+    //--------------------------------------------------------
+
+    //--------------------------------------------------------
+    // Acessors
+    //--------------------------------------------------------
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get : fn() => $this->user->name
+        );
+    }
+
+    protected function email(): Attribute
+    {
+        return Attribute::make(
+            get : fn() => $this->user->email
+        );
+    }
+
+    protected function birth_date(): Attribute
+    {
+        return Attribute::make(
+            get : fn() => $this->user->birth_date
+        );
+    }
+
+    public function gender(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->user->gender
+        );
+    }
+
+    protected function secondary_email(): Attribute
+    {
+        return Attribute::make(
+            get : fn() => $this->user->secondary_email
+        );
+    }
+    //--------------------------------------------------------
 }
