0fff3c556b55adbf22be12496c6c594c68e83aaf
samuel wingester <sam@tenet.sator>
2026-07-09 17:46:59 -0300

    Implementação do controller de resource da model Classroom

---
+++ backend/app/Http/Controllers/Web/ClassroomController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+
+use App\Models\Classroom;
+
+use App\Http\Requests\Classroom\StoreClassroomRequest;
+use App\Http\Requests\Classroom\UpdateClassroomRequest;
+
+use App\Services\Classroom\StoreClassroomService;
+use App\Services\Classroom\UpdateClassroomService;
+
+class ClassroomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $classroom = Classroom::all();
+
+        return view( 'view::classroom.index', compact( 'classroom' ) );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view( 'view::classroom.index' );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store( StoreClassroomRequest $request, StoreClassroomService $service )
+    {
+        $teacher = Classroom::find( $request->safe()->only( ['teacher_id'] ) );
+
+        $service->execute( $request->safe()->only( ['name'] ), $teacher );
+        
+        return redirect()->route( 'view::classroom.index' )
+                         ->with( 'success', 'turma criada com sucesso' );
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show( Classroom $classroom )
+    {
+        return view( 'view::classroom.show', compact( 'classroom' ) );
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit( Classroom $classroom )
+    {
+        return view( 'view::classroom.edit', compact( 'classroom' ) );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update( UpdateClassroomRequest $request, UpdateclassroomService $service , Classroom $classroom )
+    {
+        $service->execute( $request->validated(), $classroom );
+
+        return redirect()->route( 'view::classroom.index' )
+                         ->with( 'success', 'Turma atualizada com sucesso' );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy( Classroom $classroom )
+    {
+        $classroom->deleteOrFail();
+
+        return redirect()->route( 'view::classroom.index' )
+                         ->with( 'success', 'Turma deletado com sucesso' );
+    }
+}
