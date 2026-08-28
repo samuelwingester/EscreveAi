0175a7f51ea9832b4d767487fbff014917915a87
Isaac Ramos <isaacramoss.197@gmail.com>
2026-08-27 21:21:00 -0300

    botão de excluir

--- frontend/js/components/base/HeaderBase.js
+++ frontend/js/components/base/HeaderBase.js
@@ -5,12 +5,24 @@ export class HeaderBase extends HTMLElement {
               <img src="../imgs/logos/logoL.png" alt="">
               <a href="home.html"><img src="../imgs/logos/logoH.png" alt=""></a>
           </div>
-          <div class="professor">
-              <h1>Professor <span class="nome-professor"></span></h1>
-              <svg>
-                <use href="../assets/icons/sprite.svg#icon-user"></use>
-              </svg>
-          </div>
+          <details class="icon">
+            <summary>
+              <div class="professor">
+                  <h1>Professor <span class="nome-professor"></span></h1>
+                    <svg>
+                      <use href="../assets/icons/sprite.svg#icon-user"></use>
+                    </svg>
+              </div>
+            </summary>
+            <div class="dropdown">
+              <button class="btn_option">
+                <svg>
+                    <use href="../assets/icons/sprite.svg#icon-logout"></use>
+                </svg>
+                <p>Sair da Conta</p>
+              </button>
+            </div>
+          </details>
       `;
   }
 }
